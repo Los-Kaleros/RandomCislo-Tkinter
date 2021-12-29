619e2914ba4e23490d3943dc80f1ba79139ba0a6
Los-Kaleros <valekkristian2@gmail.com>
2021-12-30 00:41:45 +0100

    Tlacidlo START

--- RandomCislo-Tkinter.py
+++ RandomCislo-Tkinter.py
@@ -1,11 +1,27 @@
 from tkinter import *
+from tkinter import font
 
 
 app = Tk()
 app.geometry("450x400")
 app.title("RandomNumber")
 app.resizable(False, False)
+app.config(bg="#14e4ff")
 
+frame = Frame(app)
+frame.pack(pady=15)
+
+btn_frame = Frame(app)
+btn_frame.pack(pady=15)
+
+start_btn = Button(
+    btn_frame,
+    text = "START",
+    font = ("times 14"),
+    bg= "grey",
+
+)
+start_btn.pack(fill=BOTH, expand=TRUE)
 
 
 app.mainloop()
