f79cac6cc9b9b6850a3c793f5bcee452aa80c2ed
Los-Kaleros <valekkristian2@gmail.com>
2021-12-30 01:05:25 +0100

    def print cisel

--- RandomCislo-Tkinter.py
+++ RandomCislo-Tkinter.py
@@ -1,5 +1,15 @@
 from tkinter import *
 from tkinter import font
+import random
+from tkinter import Listbox
+
+
+def vysledok():
+    value = print(random.randint(0, 10))
+    return value
+ 
+
+
 
 
 app = Tk()
@@ -8,8 +18,26 @@ app.title("RandomNumber")
 app.resizable(False, False)
 app.config(bg="#14e4ff")
 
+
 frame = Frame(app)
 frame.pack(pady=15)
+
+lb = Listbox(
+    frame,
+    width=25,
+    height=8,
+    font=('Times', 18),
+    bd=0,
+    fg='#464646',
+    highlightthickness=0,
+    selectbackground='#a6a6a6',
+    activestyle="none",
+    
+)
+lb.pack(side=LEFT, fill=BOTH)
+
+
+    
 
 btn_frame = Frame(app)
 btn_frame.pack(pady=15)
@@ -19,9 +47,13 @@ start_btn = Button(
     text = "START",
     font = ("times 14"),
     bg= "grey",
+    command=vysledok,
 
 )
-start_btn.pack(fill=BOTH, expand=TRUE)
+start_btn.pack(fill=BOTH, expand=TRUE, side=TOP)
+
+
+
 
 
 app.mainloop()
